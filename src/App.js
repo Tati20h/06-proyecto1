545fef28bee1c6c458e4cd652917a284304c4707
Angie Natalia Canchon <ltatianahernandez@sanmateo.edu.co>
2023-12-04 19:58:25 +0100

    react master pt1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Buscador } from "./Components/Buscador";
+import { Crear } from "./Components/Crear";
+import { Listado } from "./Components/Listado";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="layout">
+        {/* cabecera*/}
+        <header className="header">
+          <div className="logo">
+            <div className="play"></div>
+          </div>
+          <h1>MisPelis</h1>
+
+        </header>
+        {/*Barra de Navegacion*/}
+        <nav className="nav">
+          <ul>
+            <li> <a href="/#">Inicio </a> </li>
+            <li> <a href="/#">Peliculas </a> </li>
+            <li> <a href="/#">Blog </a> </li>
+            <li> <a href="/#">Contacto </a> </li>
+          </ul>
+        </nav>
+
+        {/*Contenido principal*/}
+        <section className="content">
+          {/*aqui van las pelis*/}
+          <Listado />
+        </section>
+        {/*Barra lateral*/}
+        <aside className="lateral">
+          <Buscador />
+
+          <Crear />
+        </aside>
+
+        {/*Pie de pagina*/}
+        <footer className="footer">
+          &copy; Master en JavaScritp ES12 y TypeScript - <a href="https://github.com/Tati20h"> GitHub-Tati20h</a>
+        </footer>
+
+
+
+
+      </div>
+    </>
+
   );
 }
 
---
+++ src/Components/Crear.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+
+export const Crear = () => {
+
+  const tituloComponente = "Añadir pelicula";
+
+
+  const [peliState, setPeliState] = useState({
+    titulo: '',
+    descripcion: '',
+
+  });
+
+  const { titulo, descripcion } = peliState;
+
+  const conseguirDatosForm = e => {
+    e.preventDefault()
+
+    alert("formuario ")
+
+    //conseguir datos de formulario 
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+    //crear objeto de la pelicula 
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    };
+
+    //Guardar Estado 
+    setPeliState(peli)
+
+    //Guardar ene le almacenamiento local 
+    localStorage.setItem('pelis' ,  [peli])
+
+  }
+
+  return (
+    <>
+      <div className="add">
+        <h3 className="title">
+          {tituloComponente}
+        </h3>
+        <strong>
+          {(titulo && descripcion) && "Se creo la pelicula :" + titulo}
+
+        </strong>
+        <form onSubmit={conseguirDatosForm}>
+          <input
+            id="titulo"
+            type="text"
+            name="titulo"
+            placeholder="Titulo" />
+
+          <textarea
+            id="descripcion"
+            name="descripcion"
+            placeholder="Description"></textarea>
+
+          <button
+            id="save"
+            type="submit"
+            value="Guardar"
+            name='save' >
+            Guardar
+          </button>
+
+        </form>
+      </div>
+    </>
+  )
+}
